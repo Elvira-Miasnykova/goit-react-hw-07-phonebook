96caf0e9e7035695bec9e3d96159135f8266836d
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2022-11-16 16:10:15 +0200

    del

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,27 +1,25 @@
 import axios from "axios";
-import {createAsyncThunk} from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 
 axios.defaults.baseURL = 'https://636eb7eef2ed5cb047ce09e6.mockapi.io/contacts/';
-//axios.defaults.baseURL = 'https://6367cee5d1d09a8fa61b0c62.mockapi.io';
-
 
 export const fetchContacts = createAsyncThunk("contact/fetchAllContacts",
-    async (_, { rejectWithValue }) => {
-        try {
-            const response = await axios.get("/contacts");
-            return (response.data);
-        }
-        catch (e) {
-    
-            return (rejectWithValue(e.message));
-        };
-
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get("/contacts");
+      return (response.data);
     }
-); 
+    catch (e) {
+
+      return (rejectWithValue(e.message));
+    };
+
+  }
+);
 
 export const addContact = createAsyncThunk(
   "contacts/addContact",
-  async (text, {rejectWithValue}) => {
+  async (text, { rejectWithValue }) => {
     try {
       const response = await axios.post("/contacts", text);
       return response.data;
@@ -33,7 +31,7 @@ export const addContact = createAsyncThunk(
 
 export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
-  async (contactId, {rejectWithValue}) => {
+  async (contactId, { rejectWithValue }) => {
     try {
       const response = await axios.delete(`/contacts/${contactId}`);
       return response.data;
